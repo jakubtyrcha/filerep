6a9f3b5668eb777db26ae9fe04cd4dc56f2775de
Jakub <jakub@polystream.com>
2021-02-17 00:59:04 +0000

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate clap;
+use clap::{Arg, App};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::net::{TcpStream, TcpListener};
 use std::error::Error;
@@ -231,15 +233,23 @@ async fn run_server(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Er
 
 #[tokio::main]
 async fn main() {
-    let port = 12345;
-    let address = "127.0.0.1";
+    let matches = App::new("filerep")
+        .arg(Arg::with_name("server").long("server").takes_value(false))
+        .arg(Arg::with_name("port").required(true))
+        .arg(Arg::with_name("path").required(true))
+        .arg(Arg::with_name("address").required_unless("server"))
+        .get_matches();
+
+    let port = matches.value_of("port").unwrap().parse::<u16>().unwrap();
+    let path : String = String::from(matches.value_of("path").unwrap());
+    let is_server = matches.is_present("server");
+    let address = if is_server { String::from("127.0.0.1") } else { String::from(matches.value_of("address").unwrap()) };
     let socket_address = SocketAddr::new(address.parse().unwrap(), port);
-    let is_server = env::args().len() == 1;
 
     if(is_server) {
-        run_server(socket_address, Path::new("test/test_file")).await;
+        run_server(socket_address, Path::new(&path)).await;
     }
     else {
-        run_client(socket_address, Path::new("test/out_file")).await;
+        run_client(socket_address, Path::new(&path)).await;
     }
 }
