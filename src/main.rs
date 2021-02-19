a0787b81ced3007bec3387b8bcbeec03b7adb16b
Jakub <jakub@polystream.com>
2021-02-19 00:38:04 +0000

    - added comments - added client offset checks - made file notification pump return to scheduler more often

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use futures::sink::SinkExt;
 use futures::stream::StreamExt;
 use std::path::Path;
 use std::sync::Arc;
+use std::sync::mpsc::TryRecvError;
 use notify::{Watcher, RecursiveMode, watcher};
 use tokio::fs::File;
 use tokio::time::Duration;
@@ -20,44 +21,57 @@ use chunk::{FileChunkDecoder, FileChunkEncoder, FileChunk};
 mod handshake;
 use handshake::{HandshakeDecoder, HandshakeEncoder};
 
+/*
+Client sends the handshake and receives the file chunks
+*/
 async fn run_client(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Error>> {
     let stream = TcpStream::connect(address).await?;
     stream.writable().await?;
 
-    // handshake
+    // `Framed` allows to send structured data, wrap the socket and send one handshake element
     let mut handshake_enc = Framed::new(stream, HandshakeEncoder{});
     handshake_enc.send(()).await?;
+    // if the handshake is wrong, the server will terminate the connection
 
     let stream = handshake_enc.into_inner();
-
     let mut file = File::create(path).await?;
 
-    // start receiving data
     stream.readable().await?;
     let mut dec = Framed::new(stream, FileChunkDecoder{});
+    let mut current_offset = 0;
 
     loop {
         match dec.next().await {
-            Some(chunk) => {
-                file.write(&chunk.unwrap().data[..]).await?;
+            Some(Ok(chunk)) => {
+                if current_offset != chunk.offset {
+                    return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "offset mismatch").into());
+                }
+                current_offset += chunk.data.len() as u64;
+                file.write(&chunk.data[..]).await?;
             },
-            None => {
-                return Ok(());
-            }
+            Some(Err(e)) => {
+                return Err(e.into());
+            },
+            None => unreachable!()
         }
     }
 }
 
+/*
+A linked list of all incremental chunks is maintained. 
+The nodes can be read concurrently by many connection tasks.
+Only the `next` field of the tail node will be contended by the writer once it is appended.
+TODO: Is there an idiom or a crate that delivers similar functionality? This could potentially be replaced by tokio::sync::broadcast.
+*/
 struct Node {
     next : Arc<RwLock<Option<Node>>>,
-    data : Vec<u8>,
-    gen : i64
+    data : Vec<u8>
 }
 
 impl Node {
-    fn new(data : Vec<u8>, gen : i64) -> Node { 
+    fn new(data : Vec<u8>) -> Node { 
         Node { 
-            next : Arc::new(RwLock::new(None)), data, gen
+            next : Arc::new(RwLock::new(None)), data
         } 
     }
     async fn append(&mut self, node : Node) -> Arc<RwLock<Option<Node>>> {
@@ -67,13 +81,23 @@ impl Node {
     }
 }
 
+/*
+The server is more involving than the client. 
+We have tasks to: 
+1) Drain the file watch notify events. I believe Notify internally uses a thread, as it communicates 
+using std::sync::mpsc queue, so we have a sync <-> async code communication. To avoid deadlock, the 
+task yields to the scheduler after every message.
+2) Extend the linked list of file appends with a new node. This waits on notification from (1) and 
+performs async file read. 
+3) Per connection tasks that wait on changes from (2) and send the new chunk to the recipents.
+*/
 async fn run_server(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind(&address)
         .await
         .expect("unable to bind TCP listener");
 
-    let notify_file_changed = Arc::new(Notify::new());
-    let notify_file_changed_receiver = notify_file_changed.clone();
+    let file_changed_notify_tx = Arc::new(Notify::new());
+    let file_changed_notify_rx = file_changed_notify_tx.clone();
     
     let (file_tx, file_rx) = std::sync::mpsc::channel();
     // this specifies how events are debounced (grouped) if they happen within a short interval
@@ -87,59 +111,68 @@ async fn run_server(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Er
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).await?;
 
-    // this is the file changed event loop
+    // This is the task (1)
     tokio::spawn(async move {
         loop {
-            // todo: this should make sure we progressed with other work before we start handling more notifications?
             let mut write_detected = false;
-            match file_rx.recv() {
-                Ok(event) => {
-                    match event {
-                        notify::DebouncedEvent::Write(_) => {
-                            write_detected = true;
+            // We call try_recv to avoid indefinitely stalling the task in case the file is not changed.
+            // It might be better to use `recv_timeout` to avoid failing because of contention, but 
+            // according to the doc it's broken.
+            let mut drained = false;
+            while !drained && !write_detected {
+                match file_rx.try_recv() {
+                    Ok(event) => {
+                        match event {
+                            notify::DebouncedEvent::Write(_) => {
+                                write_detected = true;
+                            }
+                            _ => {}
                         }
-                        _ => {}
+                    },
+                    Err(TryRecvError::Empty) => { drained = true; },
+                    Err(TryRecvError::Disconnected) => {
+                        return Err(TryRecvError::Disconnected);
                     }
-                },
-                _ => {}
+                }
             }
 
             if write_detected {
-                notify_file_changed.notify_one();
-                tokio::task::yield_now().await;
+                file_changed_notify_tx.notify_one();
             }
+            // Return control to the scheduler
+            tokio::task::yield_now().await;
         }
+
+        #[allow(unreachable_code)]
+        Ok::<_, TryRecvError>(())
     });
 
-    let file_chunks_list_head = Arc::new(RwLock::new(Some(Node::new(buffer, 0))));
+    let file_chunks_list_head = Arc::new(RwLock::new(Some(Node::new(buffer))));
     let mut write_head = file_chunks_list_head.clone();
-    let (list_tx, list_rx) = watch::channel(0);
+    let (list_tx, list_rx) = watch::channel(());
     
+    // This is the task (2)
     tokio::spawn(
         async move {
         let mut file = Box::pin(file);
         loop {
-            notify_file_changed_receiver.notified().await;
+            file_changed_notify_rx.notified().await;
             let mut buffer = Vec::new();
             file.as_mut().read_to_end(&mut buffer).await?;
 
             if buffer.len() > 0 {
-                println!("Read new file chunk of len {}", buffer.len());
-
                 let next_head;
-                let gen;
                 {
                     let mut write_guard = write_head.write().await;
                     let node = write_guard.as_mut().unwrap();
-                    gen = node.gen + 1;
-                    next_head = node.append(Node::new(buffer, gen)).await;
+                    next_head = node.append(Node::new(buffer)).await;
                 }
                 write_head = next_head;
-                list_tx.send(gen)?;
+                list_tx.send(())?;
             }
 
         }
-        // this is here because the compiler can't infer the return type
+        // This is here because the compiler can't infer the return type
         #[allow(unreachable_code)]
         Ok::<_, Box<dyn Error + Send + Sync>>(())
     });
@@ -149,6 +182,7 @@ async fn run_server(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Er
         let mut connection_list_rx = list_rx.clone();
         let connection_list_head = file_chunks_list_head.clone();
         
+        // This is the task (3)
         tokio::spawn(async move {
             socket.readable().await?;
 
@@ -159,7 +193,6 @@ async fn run_server(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Er
 
             let socket = handshake_dec.into_inner();
 
-            // handshake succesful
             let mut read_head = connection_list_head;
             let mut framed = Framed::new(socket, FileChunkEncoder{});
             let mut offset = 0;
@@ -170,7 +203,6 @@ async fn run_server(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Er
                     let read_guard = read_head.read().await;
                     if let Some(node) = read_guard.as_ref() {
                         next_head = Some(node.next.clone());
-                        // write to socket
                         framed.send(FileChunk{ offset : offset, data : &node.data }).await?;
                         offset = offset + node.data.len() as u64;
                     }
@@ -182,7 +214,7 @@ async fn run_server(address : SocketAddr, path : &Path) -> Result<(), Box<dyn Er
                     connection_list_rx.changed().await?;
                 }
             }
-            // this is here because the compiler can't infer the return type
+            // This is here because the compiler can't infer the return type
             #[allow(unreachable_code)]
             Ok::<_, Box<dyn Error + Send + Sync>>(())
         });
